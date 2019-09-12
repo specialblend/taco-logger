029b53f61992aded975aa1e907e8293cfb66e661
CJ Salem <cj.salem@fishtech.group>
2019-09-12 14:01:36 -0500

    ignore lib and export esm module

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,14 @@ export default
 {
     input: 'src/main.js',
     output: [
+        // {
+        //     file: 'lib/index.js',
+        //     format: 'cjs',
+        // },
         {
             file: 'lib/index.js',
-            format: 'cjs',
+            format: 'esm',
         },
-        // {
-        //     file: 'lib/index.esm.js',
-        //     format: 'esm',
-        // },
         // {
         //     file: 'lib/index.umd.js',
         //     format: 'umd',
