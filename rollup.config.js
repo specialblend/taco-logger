7eda846196e4e324524157f36a58d16f1f3759cf
specialblend <cj@specialblend.net>
2019-11-13 15:10:05 -0600

    export modules

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,19 @@ export default
 {
     input: 'src/main.js',
     output: [
-        // {
-        //     file: 'lib/index.js',
-        //     format: 'cjs',
-        // },
         {
             file: 'lib/index.js',
+            format: 'cjs',
+        },
+        {
+            file: 'lib/index.esm.js',
             format: 'esm',
         },
-        // {
-        //     file: 'lib/index.umd.js',
-        //     format: 'umd',
-        //     name: 'index',
-        // },
+        {
+            file: 'lib/index.umd.js',
+            format: 'umd',
+            name: 'index',
+        },
     ],
     external: ['winston'],
 };
